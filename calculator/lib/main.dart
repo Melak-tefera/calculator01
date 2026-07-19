d5e3ce1bd5c78621141262b83b5eaa6becb3191b
Melak-tefera <tmeleak@gmail.com>
2026-07-19 16:54:19 +0100

    second  commit

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: HomePage(),
     );
   }
 }
